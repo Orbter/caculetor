c98d9bdde457f3df46e816419889948dce8bd073
Orbter <assouline39@gmail.com>
2023-12-08 21:54:45 +0200

    updated the js to show caculetore

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+class Calculatore {
+  constructor(screen) {
+    this.screen = screen;
+    this.clear();
+  }
+
+  clear() {
+    this.screen.textContent = "";
+  }
+
+  appendNumber(number) {
+    this.screen.textContent += number;
+  }
+
+  choseOperetor(operator) {
+    this.screen.textContent += ` ${operator} `;
+  }
+
+  compute() {}
+  updatedisplay() {}
+}
+
+// ... (rest of your code)
+
 const caculetorButtons = document.querySelectorAll(".button");
 const specialButtons = document.querySelectorAll(".special-dark");
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -5,7 +29,8 @@ const operitoreButtons = document.querySelectorAll("[data-operetore]");
 const equalButton = document.querySelector("[data-equls]");
 const deleatAll = document.querySelector("[data-deletAll]");
 const deleat = document.querySelector("[data-delet]");
-
+const screen = document.querySelector("[deta-screen]");
+const calculatore = new Calculatore(screen);
 function buttonAnimation(button) {
   // Store the initial background color
   const initialColor = button.style.backgroundColor;
@@ -30,10 +55,22 @@ function resetButtonColor(button) {
 // Attach the event listener to each button
 caculetorButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    buttonAnimation(this); // "this" refers to the clicked button
-    // Do other actions or calculations related to the button click
+    buttonAnimation(this);
   });
   button.addEventListener("mouseleave", function () {
     resetButtonColor(this);
+  });
+});
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculatore.appendNumber(button.innerText);
+    calculatore.updatedisplay();
+  });
+});
+
+operitoreButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculatore.choseOperetor(button.innerText);
+    calculatore.updatedisplay();
   });
 });
